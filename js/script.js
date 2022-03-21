e2cc7644a5f981f20e98e1bcf2a666bc1ad24fc9
Charmaine Lee <charmainelhm@gmail.com>
2022-03-21 15:49:41 +0800

    Initialise repository

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const backToTop = document.querySelector(".btt-btn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTop.style.display = "block";
+  } else {
+    backToTop.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
